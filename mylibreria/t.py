4dc4976a87c8558239a1880ac7ecebc3be9ffb0c
embolao <kent@embolao.com>
2021-02-26 20:54:04 +0000

    Pytest parametrize

--- mylibreria/t.py
+++ mylibreria/t.py
@@ -1,10 +1,40 @@
-import sys
+from typing import NamedTuple
+import pytest
 
-print(f'{sys.stdin.isatty()=}')
-print(f'{sys.stdout.isatty()=}')
-print(f'{sys.stderr.isatty()=}')
 
-print('foo', file=sys.stdout)
-print('bar', file=sys.stderr)
+class Case(NamedTuple):
+    input_x: int
+    expected: int
 
-print(f'received input: {input("enter your name: ")}')
+
+def square(n: int) -> int:
+    """docstring for square"""
+    return n * n
+
+
+@pytest.mark.parametrize(
+    ('input_x', 'expected'),
+    (
+        (1, 1),
+        pytest.param(-1, 1, id='negative_trivial_case'),
+        Case(input_x=2, expected=4),
+        (-2, 4),
+    ),
+)
+def test_square(input_x, expected):
+    """docstring for test_square"""
+    assert square(input_x) == expected
+
+
+@pytest.mark.parametrize(
+    'input_x',
+    (
+        'a',
+        [],
+        (),
+    ),
+)
+def test_square_error(input_x):
+    """docstring for test_square_error"""
+    with pytest.raises(TypeError):
+        square(input_x)
